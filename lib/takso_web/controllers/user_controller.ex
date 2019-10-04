c0b22960bb1cd637601b549bf6ad0645227450cf
Miras <mikenz@ttu.ee>
2019-10-04 14:14:43 +0300

    Added edit and update

--- lib/takso_web/controllers/user_controller.ex
+++ lib/takso_web/controllers/user_controller.ex
@@ -1,7 +1,36 @@
 defmodule TaksoWeb.UserController do
   use TaksoWeb, :controller
+  alias Takso.Repo
+  alias Takso.Accounts.User
 
   def index(conn, _params) do
-    render conn, "index.html"
+    users = Repo.all(User)
+    render(conn, "index.html", users: users)
+  end
+
+  def new(conn, _params) do
+    changeset = User.changeset(%User{}, %{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+
+    Repo.insert(changeset)
+    redirect(conn, to: Routes.user_path(conn, :index))
+  end
+
+  def edit(conn, %{"id" => id}) do
+    user = Repo.get!(User, id)
+    changeset = User.changeset(user, %{})
+    render(conn, "edit.html", user: user, changeset: changeset)
+  end
+
+  def update(conn, %{"id" => id, "user" => user_params}) do
+    user = Repo.get!(User, id)
+    changeset = User.changeset(user, user_params)
+
+    Repo.update(changeset)
+    redirect(conn, to:  Routes.user_path(conn, :index))
   end
 end
